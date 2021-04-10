3e7e9722c54ff71ef8f61f94424d2198b0b1a769
StephenTerror <2684109034@qq.com>
2021-04-10 18:59:59 +0800

    fix auto_encoder

--- core/models/auto_encoder/vgg_like.py
+++ core/models/auto_encoder/vgg_like.py
@@ -61,22 +61,111 @@ class VGG_Encoder(nn.Module):
         return x
 
 
+class NetG(nn.Module):
+    def __init__(self, nc=3, nef=64, ngf=64, nBottleneck=4000, ngpu=1):
+        super(NetG, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 128 x 128
+            nn.Conv2d(nc, nef, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (nef) x 64 x 64
+            nn.Conv2d(nef, nef, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(nef),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (nef) x 32 x 32
+            nn.Conv2d(nef, nef * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(nef * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (nef*2) x 16 x 16
+            nn.Conv2d(nef * 2, nef * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(nef * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (nef*4) x 8 x 8
+            nn.Conv2d(nef * 4, nef * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(nef * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (nef*8) x 4 x 4
+            nn.Conv2d(nef * 8, nBottleneck, 4, bias=False),
+            # tate size: (nBottleneck) x 1 x 1
+            nn.BatchNorm2d(nBottleneck),
+            nn.LeakyReLU(0.2, inplace=True),
+            # input is Bottleneck, going into a convolution
+            nn.ConvTranspose2d(nBottleneck, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
+
+class NetG32(nn.Module):
+    def __init__(self, nc=3, nef=64, ngf=64, nBottleneck=4000, ngpu=1, **kwargs):
+        super(NetG32, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # 32 => 16
+            nn.Conv2d(nc, nef, 4, 2, 1, bias=False), nn.LeakyReLU(0.2, inplace=True),
+            # 16 => 8
+            nn.Conv2d(nef, nef*2, 4, 2, 1, bias=False), nn.BatchNorm2d(nef*2), nn.LeakyReLU(0.2, inplace=True),
+            # 8 => 4
+            nn.Conv2d(nef*2, nef * 4, 4, 2, 1, bias=False), nn.BatchNorm2d(nef * 4), nn.LeakyReLU(0.2, inplace=True),
+            # 4 => 1
+            nn.Conv2d(nef * 4, nBottleneck, 4, bias=False), nn.BatchNorm2d(nBottleneck), nn.LeakyReLU(0.2, inplace=True),
+            # 1 => 4
+            nn.ConvTranspose2d(nBottleneck, ngf * 4, 4, 1, 0, bias=False), nn.BatchNorm2d(ngf * 4), nn.ReLU(True),
+            # 4 => 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False), nn.BatchNorm2d(ngf * 2), nn.ReLU(True),
+            # 8 => 16
+            nn.ConvTranspose2d(ngf * 2, ngf * 1, 4, 2, 1, bias=False), nn.BatchNorm2d(ngf * 1), nn.ReLU(True),
+            # 16 => 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False), nn.Tanh()
+            # output == (nc) x 32 x 32
+        )
+
+    def forward(self, inputs):
+        output = self.main(inputs)
+        return output
+
+
+# def get_vgg_encoder(**kwargs):
+#     return VGG_Encoder(**kwargs)
+
 def get_vgg_encoder(**kwargs):
-    return VGG_Encoder(**kwargs)
+    return NetG32(**kwargs)
 
 
 if __name__ == '__main__':
-
     input_tensor = torch.randn((4, 3, 32, 32), dtype=torch.float32)
     # model = get_vgg_encoder()
-    model = nn.ConvTranspose2d(3, 6, 3, stride=2, padding=1, output_padding=1)
+    model = NetG32()
     print(model(input_tensor).shape)
 
-
-    encoder = Encoder()
-    out = encoder(input_tensor)
-    print(out.shape)
-
-    decoder = Decoder()
-    out = decoder(out)
-    print(out.shape)
+    # encoder = Encoder()
+    # out = encoder(input_tensor)
+    # print(out.shape)
+    #
+    # decoder = Decoder()
+    # out = decoder(out)
+    # print(out.shape)
